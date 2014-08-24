eda14b871fdb9f5b0d28b5ca4e6624efa4a8e7cd
金子修一郎 <kanekoshuuichirou@kanekosrounombp.lan>
2014-08-24 23:12:38 +0900

    Initial Commit

---
+++ iOSTraining9-3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSTraining9-3
+//
+//  Created by 金子修一郎 on 2014/08/24.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSTraining9-3Tests/iOSTraining9_3Tests.m
@@ -0,0 +1,34 @@
+//
+//  iOSTraining9_3Tests.m
+//  iOSTraining9-3Tests
+//
+//  Created by 金子修一郎 on 2014/08/24.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSTraining9_3Tests : XCTestCase
+
+@end
+
+@implementation iOSTraining9_3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
